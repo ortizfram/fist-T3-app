66bb740d127f7a45e82ccb912aa6f26278dfe9d6
melon <ortizfranco48@gmail.com>
2023-04-22 11:23:57 -0300

    router

--- my-t3-app/src/pages/index.tsx
+++ my-t3-app/src/pages/index.tsx
@@ -1,14 +1,25 @@
 import { SignOutButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { NextPage } from 'next'
 
-function Index() {
+import { api } from '../utils/api'
+
+const Home: NextPage = () => {
   const user = useUser();
 
+  // get posts from backend
+  const { data } = api.posts.getAll.useQuery();
+
   return (
-    <div>
-      {!user.isSignedIn && <SignUpButton />}
-      {user.isSignedIn && <SignOutButton />}
-    </div>
+    <>
+      <div>
+        {!user.isSignedIn && <SignUpButton />}
+        {user.isSignedIn && <SignOutButton />}
+      </div>
+      <div>
+        {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+      </div>
+    </>
   );
 }
 
-export default Index;
+export default Home;
