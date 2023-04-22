38f88ca3ab22ab7e6f5ca2fd377972fa0c72340f
melon <ortizfranco48@gmail.com>
2023-04-22 12:26:22 -0300

    style skeleton

--- my-t3-app/src/pages/index.tsx
+++ my-t3-app/src/pages/index.tsx
@@ -6,18 +6,24 @@ import { api } from '../utils/api'
 const Home: NextPage = () => {
   const user = useUser();
 
-  // get posts from backend
-  const { data } = api.posts.getAll.useQuery();
+
+  const { data, isLoading } = api.posts.getAll.useQuery();
+  if (isLoading) return <div>Loading...</div>
+  if (!data) return <div>Something went wrong</div>
 
   return (
     <>
-      <div>
-        {!user.isSignedIn && <SignUpButton />}
-        {user.isSignedIn && <SignOutButton />}
-      </div>
-      <div>
-        {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
-      </div>
+      <main className="flex justify-center h-screen">
+        <div className="border border-x border-slate-400 w-full md:max-w-2xl">
+          <div className="flex border-b border-slate-200 p-4">
+            {!user.isSignedIn && <div className="flex justify-center"><SignUpButton /></div>}
+            {user.isSignedIn && <SignOutButton />}
+          </div>
+          <div className="flex flex-col">
+            {[...data, ...data]?.map((post) => (<div key={post.id} className="p-8 border-b border-slate-400">{post.content}</div>))}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
